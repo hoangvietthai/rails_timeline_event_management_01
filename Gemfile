341f8bf7a10ed6e2fd93c806feb92f5f1353c868
peiguangda <peiguangda@gmail.com>
2018-07-24 17:26:29 +0700

    activation + reset

--- Gemfile
+++ Gemfile
@@ -17,8 +17,9 @@ gem "rails", "~> 5.2.0"
 gem "sass-rails", "~> 5.0"
 gem "turbolinks", "~> 5"
 gem "uglifier", ">= 1.3.0"
-gem "kaminari"
+
 gem "bootstrap-kaminari-views"
+gem "kaminari"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
-  root "sessions#new"
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
+  root "static_pages#home"
   get "/signup", to: "users#new"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users
+  resources :account_activations, only: :edit
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
